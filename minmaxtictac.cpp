105b6ec439b81b1812529a51dd6d016fc2079d5b
Satyam-Muluk <73930297+Satyam-Muluk@users.noreply.github.com>
2021-09-27 17:27:38 +0530

    Add files via upload

---
+++ minmaxtictac.cpp
@@ -0,0 +1,268 @@
+#include <iostream>
+using namespace std;
+
+//int ply = 0;
+class moves
+{   public:
+    int r, c, score;
+};
+class TicTacToe
+{  
+    public:
+    char player, computer, grid[3][3];
+    TicTacToe()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                grid[i][j] = ' ';
+            }
+        }
+    }
+
+    void player_move()
+    {
+        while (true)
+        {
+            cout << "Enter an empty cell (1-9)";
+            int cell;
+            cin >> cell;
+            int r = (cell - 1) / 3;
+            int c = (cell - 1) % 3;
+
+            if (cell >= 1 && cell <= 9 && grid[r][c] == ' ')
+            {
+                grid[r][c] = player;
+                break;
+            }
+        }
+    }
+
+    bool win()
+    {
+        
+
+        for (int i = 0; i < 3; i++)
+        {
+            if (grid[i][0] == grid[i][1] &&
+                grid[i][1] == grid[i][2] &&
+                grid[i][0] != ' ')
+                return (true);
+
+           
+        }
+         for (int i = 0; i < 3; i++)
+            {
+                if (grid[0][i] == grid[1][i] &&
+                    grid[1][i] == grid[2][i] &&
+                    grid[0][i] != ' ')
+                    return (true);
+            }
+         if (grid[0][0] == grid[1][1] &&
+        grid[1][1] == grid[2][2] && 
+        grid[0][0] != ' ')
+        return(true);
+          
+    if (grid[0][2] == grid[1][1] &&
+        grid[1][1] == grid[2][0] &&
+         grid[0][2] != ' ')
+        return(true);
+    return false;
+    }
+
+    bool tie()
+    {
+        if (win())
+            return false;
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (grid[i][j] == ' ')
+                {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    void play()
+    {
+        while (true)
+        {
+            cout << "Which symbol (X or O, X will play first)? ";
+            cin >> player;
+            if (player == 'X' || player == 'O')
+            {
+                break;
+            }
+        }
+        if(player=='X')
+        {
+            computer= 'O';
+        }
+        else
+        {
+            computer='X';
+        }
+      
+        if (player == 'O')
+        {
+            computer_move();
+        }
+        print();
+        while (true)
+        {
+            player_move();
+            print();
+            if (win())
+            {
+                cout << "Player wins!\n";
+                return;
+            }
+            else if (tie())
+            {
+                cout << "Tie!\n";
+                return;
+            }
+            cout << "Computer is making a move...\n";
+            computer_move();
+            print();
+            if (win())
+            {
+                cout << "Computer wins!\n";
+                return;
+            }
+            else if (tie())
+            {
+                cout << "Tie!\n";
+                return;
+            }
+        }
+    }
+    void print()
+    {
+        cout << '\n';
+        for (int i = 0; i < 3; i++)
+        {
+            if (i)
+            {
+                cout << "-----------\n";
+            }
+            for (int j = 0; j < 3; j++)
+            {
+                if (j)
+                {
+                    std::cout << "|";
+                }
+                cout << ' ';
+                if (grid[i][j] == ' ')
+                {
+
+                    cout << " ";
+                }
+                else
+                {
+                    cout << grid[i][j];
+                }
+                cout << ' ';
+            }
+            cout << '\n';
+        }
+        cout << '\n';
+    }
+
+    moves minimax(bool maximizing_player,int ply)
+    {
+        moves best_move;
+        if(ply==2)
+        {
+            return best_move;
+        }
+        if (win())
+        {
+            if (maximizing_player) //  player won so computer lost so return -1
+            {
+
+                best_move.score = -1;
+            }
+            else
+            {
+                best_move.score = 1;
+            }
+            return best_move;
+        }
+        else if (tie())
+        {
+            best_move.score = 0;
+            return best_move;
+        }
+
+        
+        if(maximizing_player)
+        {
+            best_move.score = -2;
+        }
+        else
+        {
+            best_move.score  = 2;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (grid[i][j] == ' ')
+                {  
+                    if(maximizing_player)
+                    {
+                        grid[i][j] = computer;
+                    }
+                    else
+                    {
+                        grid[i][j] = player;
+                    }
+                   // grid[i][j] = maximizing_player ? computer : player;
+                   // print();
+                    moves board_state = minimax(!maximizing_player,ply+1);
+                    if (maximizing_player)
+                    {
+                        if (board_state.score > best_move.score)
+                        {
+                            best_move.score = board_state.score;
+                            best_move.r = i;
+                            best_move.c = j;
+                        }
+                    }
+                    else
+                    {
+                        if (board_state.score < best_move.score)
+                        {
+                            best_move.score = board_state.score;
+                            best_move.r = i;
+                            best_move.c = j;
+                        }
+                    }
+                    grid[i][j] = ' ';
+                }
+            }
+        }
+        // cout<<best_move.score<<endl;
+        return best_move;
+    }
+    void computer_move()
+    {
+        moves best_move = minimax(true,0);
+        cout << "Score:" << best_move.score << endl;
+        cout << "Row :" << best_move.r << " ";
+        cout << "Column:" << best_move.c << endl;
+        grid[best_move.r][best_move.c] = computer;
+    }
+};
+
+int main()
+{
+    TicTacToe game;
+    game.play();
+}
